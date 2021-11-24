9795d6b371da95f1bd7809de3e1f3a719b80cec0
Jabi Habash <jabi.habash@gmail.com>
2021-11-24 07:19:02 -0500

    feat: Implements asp.net api to get shipping prices

---
+++ src/api/ShoppingCart/ShoppingCart/Controllers/ShippingPricesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShoppingCart.Models;
+using ShoppingCart.Services;
+
+namespace ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShippingPricesController : ControllerBase
+    {
+        private IShippingPricesService shippingPricesService;
+
+        public ShippingPricesController(IShippingPricesService shippingPricesService)
+        {
+            this.shippingPricesService = shippingPricesService;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<ShippingPrice>> Get() => 
+            await shippingPricesService.GetAllShippingPricesAsync();
+
+    }
+}
---
+++ src/api/ShoppingCart/ShoppingCart/ServiceExtentions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ShoppingCart
+{
+    public static class ServiceExtentions
+    {
+        public static void ConfigureCors(this IServiceCollection services, string policyName)
+        {
+            services.AddCors(options => {
+                options.AddPolicy(policyName,
+                    builder => builder.AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+            });
+        }
+
+        public static void RegisterServicesIoc(this IServiceCollection services) 
+        {
+            services.AddScoped<ShoppingCart.Services.IShippingPricesService, ShoppingCart.Services.ShippingPricesService>();
+        }
+    }
+}
